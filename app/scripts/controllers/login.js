6865bab959436e1ff4b69e389dcfb7ab244c897c
fooles <18730471450@163.com>
2017-08-03 15:11:30 +0800

    login  route

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,6 +2,16 @@
  * Created by sorcerer on 2017/8/3.
  */
 angular.module('basic')
-  .controller('loginCtrl',[function () {
+  .controller('loginCtrl',['login','$scope','Cookie',function (login,$scope,Cookie) {
     //$rootScope.tab = "service";
+    $scope.usermessage={
+      username:'',
+      password:''
+    }
+    $scope.gologin= function () {
+      login.post($scope.usermessage, function (data) {
+        console.log('data', data);
+        Cookie.set('token', data.token, 24 * 3600 * 1000);
+      })
+    }
   }]);
