ccbe28a90babb32e683b81e04620c73146fb9ab3
fooles <18730471450@163.com>
2017-08-17 15:29:32 +0800

    user

--- localstart.sh
+++ localstart.sh
@@ -1,4 +1,4 @@
 export ADAPTER_API_SERVER='http://10.1.236.113:9090'
-export SVCAMOUNT_API_SERVER='http://svc-amount.dev-citic.dataos.io'
+export SVCAMOUNT_API_SERVER='http://192.168.1.125:8080'
 export RELEASE_EDITION='dev'
 gulp serve
